0b96f156b8a672eb80205a61021502e853168621
Avgurs <seregepochta@gmail.com>
2018-05-20 01:37:50 +0300

    Добавьте файлы проекта.

---
+++ OverLoadOperators/ThreeD.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadOperators
+{
+    class ThreeD
+    {
+        int x, y, z;
+
+        public ThreeD()
+        {
+            x = y = z = 0;
+        }
+       
+        public ThreeD(int i, int j, int k)
+        {
+            x = i;
+            y = j;
+            z = k;
+        }
+
+        public static ThreeD operator + (ThreeD op1, ThreeD op2)
+        {
+            ThreeD result = new ThreeD();
+            result.x = op1.x + op2.x;
+            result.y = op1.y + op2.y;
+            result.z = op1.z + op2.z;
+            return result;
+        }
+
+        public static ThreeD operator +(ThreeD op1, int op2)
+        {
+            ThreeD result = new ThreeD();
+            result.x = op1.x + op2;
+            result.y = op1.y + op2;
+            result.z = op1.z + op2;
+            return result;
+        }
+
+        static public ThreeD Fabrica(int i, int j, int k)
+        {
+            ThreeD t = new ThreeD(i, j, k);
+            return t;
+        }
+
+        static public ThreeD Fabrica()
+        {
+            ThreeD t = new ThreeD();
+            return t;
+        }
+        public void Show()
+        {
+            Console.WriteLine("x = {0}; y  = {1} z = {2}", x, y, z);
+        }
+
+    }
+}
---
+++ OverLoadOperators/ThreeDDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadOperators
+{
+    class ThreeDDemo
+
+    {
+        static void Main(string[] args)
+        {
+            ThreeD ob1 = ThreeD.Fabrica(1, 2, 5);
+            ThreeD ob2 = ThreeD.Fabrica(5, -5, 10);
+            ThreeD ob3 = ob1 + ob2;
+            ob3.Show();
+            Console.ReadKey();
+        }
+
+    }
+}
